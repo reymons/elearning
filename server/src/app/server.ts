48cb1cbb3a8f80cbdd1e68d9b6aee1039bf478a1
Daniel <reymonsma@gmail.com>
2025-06-19 10:33:50 +0200

    init: add https support for dev environment

--- server/src/app/server.ts
+++ server/src/app/server.ts
@@ -1,10 +1,21 @@
-import Fastify from "fastify";
+import path from "path";
+import fs from "fs";
+import Fastify, { type FastifyServerOptions } from "fastify";
 import { registerPlugins } from "./plugins";
+import { MODE } from "@/config/env";
 
 export async function buildServer() {
-    const fastify = Fastify({
+    const opts: FastifyServerOptions = {
         logger: true,
-    });
+    };
+    if (MODE === "dev") {
+        (opts as any).https = {
+            key: fs.readFileSync(path.join(__dirname, "tls", "key.pem")),
+            cert: fs.readFileSync(path.join(__dirname, "tls", "cert.pem")),
+        };
+    }
+
+    const fastify = Fastify(opts);
 
     registerPlugins(fastify);
 
--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,4 +1,4 @@
+export const MODE = "dev";
 export const ALLOWED_ORIGINS = ["https://localhost:5454"]
 export const HOST = "127.0.0.1";
 export const PORT = 6464;
-
